5214019d5ab20c81db520794e524ef5346cbf4c3
Shaojung <shaojung@gmail.com>
2016-10-05 16:00:34 +0800

    Use ArrayAdapter .

--- app/src/main/java/com/test/tt100504/MainActivity.java
+++ app/src/main/java/com/test/tt100504/MainActivity.java
@@ -4,17 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner sp;
+    Spinner sp, sp2;
     TextView tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sp = (Spinner) findViewById(R.id.spinner);
+        sp2 = (Spinner) findViewById(R.id.spinner2);
         tv2 = (TextView) findViewById(R.id.textView2);
 
         sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -32,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        String cities[] = {"台北","台中","高雄","台南"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                                                android.R.layout.simple_list_item_1,
+                                                cities);
+        sp2.setAdapter(adapter);
 
     }
 }
