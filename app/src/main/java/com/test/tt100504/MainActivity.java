5bdd441bbf686af26b6ea95f637a52244696d737
Shaojung <shaojung@gmail.com>
2016-10-05 15:34:54 +0800

    Get the Select Item.

--- app/src/main/java/com/test/tt100504/MainActivity.java
+++ app/src/main/java/com/test/tt100504/MainActivity.java
@@ -2,12 +2,33 @@ package com.test.tt100504;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner sp;
+    TextView tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sp = (Spinner) findViewById(R.id.spinner);
+        tv2 = (TextView) findViewById(R.id.textView2);
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView tv = (TextView) view;
+                tv2.setText(tv.getText().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
